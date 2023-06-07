e3be7cc0561184088571f6df4333e4c7002bc184
AlexLeoTW <alex@alexleo.click>
2023-06-07 17:38:44 +0800

    [feat] [failed] listen to `webRequest`

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,13 @@
+import browser from "webextension-polyfill";
+
 function polling() {
   setInterval(() => console.log("polling"), 1000 * 30);
 }
 polling();
+
+browser.webRequest.onBeforeRequest.addListener(
+  (requestDetails) => console.log(`Loading: ${requestDetails.url}`),
+  { urls: ["<all_urls>"] }
+);
+
+console.log("background.ts");
